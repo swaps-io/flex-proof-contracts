7d9a33c32539f52fdce2d97b79309ef6d82960e0
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2025-04-08 17:28:05 +0400

    Overhaul save event verifier

--- contracts/providers/save/SaveEventVerifier.sol
+++ contracts/providers/save/SaveEventVerifier.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.26;
 
 import {EventHashLib} from "../../libraries/EventHashLib.sol";
 
-import {SaveEventProofLib, SaveEventProof} from "./SaveEventProofLib.sol";
+import {SaveEventProofLib} from "./SaveEventProofLib.sol";
 
 import {ISaveEventVerifier, IEventVerifier} from "./interfaces/ISaveEventVerifier.sol";
 
@@ -24,14 +24,16 @@ contract SaveEventVerifier is ISaveEventVerifier {
         bytes calldata data_,
         bytes calldata proof_
     ) external {
-        bytes32 eventHash = EventHashLib.calc(chain_, emitter_, topics_, data_);
-        if (eventSaved[eventHash]) return; // Valid event saved prior
-
-        SaveEventProof calldata saveProof = SaveEventProofLib.decode(proof_);
-        if (saveProof.flags & 2 != 0) revert EventNotSaved(eventHash); // #1: no verify attempt after missing save
-
-        IEventVerifier(eventVerifier).verifyEvent(chain_, emitter_, topics_, data_, SaveEventProofLib.consume(proof_));
-
-        if (saveProof.flags & 1 != 0) eventSaved[eventHash] = true; // #0: should save verified
+        uint256 flags = SaveEventProofLib.decode(proof_).flags;
+        if (flags & 1 == 0) { // #0: should perform event verification (else: use saved event)
+            bytes32 eventHash = EventHashLib.calc(chain_, emitter_, topics_, data_);
+            require(eventSaved[eventHash], EventNotSaved(eventHash));
+        } else {
+            IEventVerifier(eventVerifier).verifyEvent(chain_, emitter_, topics_, data_, SaveEventProofLib.consume(proof_));
+            if (flags & 2 != 0) { // #1: should perform save of verified event
+                bytes32 eventHash = EventHashLib.calc(chain_, emitter_, topics_, data_);
+                eventSaved[eventHash] = true;
+            }
+        }
     }
 }
--- contracts/providers/save/interfaces/SaveEventProof.sol
+++ contracts/providers/save/interfaces/SaveEventProof.sol
@@ -3,5 +3,5 @@
 pragma solidity ^0.8.26;
 
 struct SaveEventProof {
-    uint256 flags; // #0: should save verified, #1: no verify attempt after missing save
+    uint256 flags; // #0: should perform event verification, #1: should perform save of verified event
 }
