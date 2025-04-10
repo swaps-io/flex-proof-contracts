81d69a33273b2dd71824a6794d374437691709e2
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2025-04-10 12:30:29 +0400

    Implement threshold check

--- contracts/providers/hashi/ShoyuBashiFossil.sol
+++ contracts/providers/hashi/ShoyuBashiFossil.sol
@@ -8,11 +8,15 @@ import {IShoyuBashiFossil} from "./interfaces/IShoyuBashiFossil.sol";
 
 contract ShoyuBashiFossil is IShoyuBashiFossil {
     uint256 public immutable threshold;
+    uint256 public immutable totalAdapters;
 
     address[] private _adapters;
 
     constructor(uint256 threshold_, address[] memory adapters_) {
+        require(adapters_.length > 0);
+        require(threshold_ > 0 && threshold_ <= adapters_.length);
         threshold = threshold_;
+        totalAdapters = adapters_.length;
         _adapters = adapters_;
     }
 
@@ -21,12 +25,26 @@ contract ShoyuBashiFossil is IShoyuBashiFossil {
     }
 
     function getThresholdHash(uint256 domain_, uint256 id_) external view returns (bytes32) {
-        uint256 adaptersLength = _adapters.length;
-        bytes32[] memory hashes = new bytes32[](adaptersLength);
-        for (uint256 i = 0; i < adaptersLength; i++) {
+        bytes32[] memory hashes = new bytes32[](totalAdapters);
+        for (uint256 i = 0; i < totalAdapters; i++) {
             hashes[i] = IAdapter(_adapters[i]).getHash(domain_, id_);
         }
 
-        revert(); // TODO: implement threshold logic
+        // Threshold check is based on `ShuSo._getThresholdHash` implementation
+        for (uint256 i = 0; i < totalAdapters; i++) {
+            if (i > totalAdapters - threshold) break;
+
+            bytes32 baseHash = hashes[i];
+            if (baseHash == bytes32(0)) continue;
+
+            uint256 num = 0;
+            for (uint256 j = i; j < totalAdapters; j++) {
+                if (baseHash == hashes[j]) {
+                    num++;
+                    if (num == threshold) return hashes[i];
+                }
+            }
+        }
+        revert ThresholdNotMet();
     }
 }
--- contracts/providers/hashi/interfaces/IShoyuBashiFossil.sol
+++ contracts/providers/hashi/interfaces/IShoyuBashiFossil.sol
@@ -5,7 +5,11 @@ pragma solidity ^0.8.26;
 import {IMinShoyuBashi} from "./IMinShoyuBashi.sol";
 
 interface IShoyuBashiFossil is IMinShoyuBashi {
+    error ThresholdNotMet();
+
     function threshold() external view returns (uint256);
+
+    function totalAdapters() external view returns (uint256);
 
     function adapters() external view returns (address[] memory);
 }
