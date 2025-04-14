098ec8109d9445e60769451cf6868f92f5b02f11
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2025-04-14 15:30:54 +0400

    Remove save & self verifiers

--- contracts/providers/save/interfaces/SaveEventProof.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-
-pragma solidity ^0.8.26;
-
-struct SaveEventProof {
-    uint256 flags; // #0: should perform event verification, #1: should perform save of verified event
-}
--- contracts/test/SelfEventVerifierTest.sol
+++ contracts/test/SelfEventVerifierTest.sol
@@ -2,9 +2,11 @@
 
 pragma solidity ^0.8.26;
 
-import {SelfEventVerifier} from "../providers/self/SelfEventVerifier.sol";
+import {IEventVerifier} from "../interfaces/IEventVerifier.sol";
 
-contract SelfEventVerifierTest is SelfEventVerifier {
+contract SelfEventVerifierTest is IEventVerifier {
+    error UnexpectedEventChain();
+    error UnexpectedEventEmitter();
     error UnexpectedEventSignature();
     error UnexpectedEventArgument();
     error UnexpectedEventDataHash();
@@ -14,17 +16,23 @@ contract SelfEventVerifierTest is SelfEventVerifier {
     uint256 public constant EXPECTED_EVENT_ARGUMENT = 133713371337;
     bytes32 public constant EXPECTED_EVENT_DATA_HASH = keccak256('test-event-data');
 
-    function _selfVerifyEvent(
+    function verifyEvent(
+        uint256 chain_,
+        address emitter_,
         bytes32[] calldata topics_,
         bytes calldata data_,
         bytes calldata proof_
-    ) internal pure override {
+    ) external view {
+        require(chain_ == block.chainid, UnexpectedEventChain());
+        require(emitter_ == address(this), UnexpectedEventEmitter());
+
         if (topics_[0] == EXPECTED_EVENT_SIGNATURE) {
             require(topics_[1] == bytes32(EXPECTED_EVENT_ARGUMENT), UnexpectedEventArgument());
             require(keccak256(data_) == EXPECTED_EVENT_DATA_HASH, UnexpectedEventDataHash());
             require(proof_.length == 0, UnexpectedEventProofLength());
             return;
         }
+
         revert UnexpectedEventSignature();
     }
 }
