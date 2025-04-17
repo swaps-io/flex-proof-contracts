5ff8902586384db5298a0eeb043f334ddfb97a7b
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2025-04-17 14:03:28 +0400

    Get rid of custom Hashi light client contracts

--- contracts/providers/hashi/receipt/interfaces/IHashiLightClient.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-
-pragma solidity ^0.8.26;
-
-interface IHashiLightClient {
-    function headers(uint256 blockNumber) external view returns (bytes32);
-}
--- contracts/providers/hashi/receipt/interfaces/IHashiReceiptShoyuBashi.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-
-pragma solidity ^0.8.26;
-
-// Part of `IShoyuBashi` interface required for `HashiProverLib.verifyForeignEvent` functionality
-
-interface IHashiReceiptShoyuBashi {
-    function getThresholdHash(uint256 domain, uint256 id) external view returns (bytes32);
-}
