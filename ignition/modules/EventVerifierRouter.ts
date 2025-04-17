21ebb995f34ad8bf88a0c44dabf0eff1acaee0f0
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2025-04-17 18:11:45 +0400

    Remove ignition module

--- ignition/modules/EventVerifierRouter.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
-
-export default buildModule('EventVerifierRouter', (m) => {
-  const account = m.getAccount(0);
-
-  const router = m.contract('EventVerifierRouter', [account]);
-
-  return { router };
-});
